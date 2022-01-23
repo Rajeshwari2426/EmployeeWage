c13a4d142a1e1f62929c4e014f61af8f336f0c0f
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-01-23 23:32:43 +0530

    adding employee attendence

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -11,6 +11,18 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Employee Wage Computation Program on Master Branch");
+
+            int isPresent = 1;
+            Random random= new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isPresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
             Console.ReadLine();
         }
     }
