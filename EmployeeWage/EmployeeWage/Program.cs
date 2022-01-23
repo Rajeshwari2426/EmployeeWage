93189179879c8d0c6c940756e5f8788c58460fc0
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-01-24 00:12:40 +0530

    adding Switch case

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -12,26 +12,23 @@ namespace EmployeeWage
         {
             Console.WriteLine(" Welcome to Employee Wage Computation Program on Master Branch");
 
-            int isFullTime = 1;
-            int isPartTime = 2;
+            const int isFullTime = 1;
+            const int isPartTime = 2;
             int empRatePerHr = 20;
             int empHrs = 0, empWage = 0;
             Random random= new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == isFullTime)
+           switch (empCheck)
             {
-                Console.WriteLine("Employee is present FullTime ");
-                empHrs = 8;
-            }
-            else if (empCheck == isPartTime)
-            {
-                Console.WriteLine("Employee is present PartTime");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-                empHrs = 0;
+                case isFullTime:
+                    empHrs = 8;
+                    break;
+                    case isPartTime:
+                    empHrs = 4;
+                    break;
+                    default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * empRatePerHr;
             Console.WriteLine("Employee wage is : " + empWage);
